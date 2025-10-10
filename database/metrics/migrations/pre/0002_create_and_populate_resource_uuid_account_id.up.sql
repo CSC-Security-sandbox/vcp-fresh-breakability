715eebb3fadc2493e90c58295f01dd3baf71012f
chitturi-shyamprasad <shyamprasad.chitturi@netapp.com>
2025-10-10 16:45:57 +0530

    VSCP-2592: Fix for metrics db pre migration (#1646)

--- database/metrics/migrations/pre/0002_create_and_populate_resource_uuid_account_id.up.sql
+++ database/metrics/migrations/pre/0002_create_and_populate_resource_uuid_account_id.up.sql
@@ -1,24 +1,45 @@
 DO $$
 BEGIN
     -- Add resource_uuid column if it does not exist
+    IF EXISTS (
+    SELECT 1 FROM information_schema.tables
+    WHERE table_name='aggregated_usages'
+) THEN
     IF NOT EXISTS (
         SELECT 1 FROM information_schema.columns
         WHERE table_name='aggregated_usages' AND column_name='resource_uuid'
     ) THEN
         EXECUTE 'ALTER TABLE aggregated_usages ADD COLUMN resource_uuid varchar(255);';
 END IF;
+END IF;
 
     -- Add account_id column if it does not exist
+IF EXISTS (
+    SELECT 1 FROM information_schema.tables
+    WHERE table_name='aggregated_usages'
+) THEN
     IF NOT EXISTS (
         SELECT 1 FROM information_schema.columns
         WHERE table_name='aggregated_usages' AND column_name='account_id'
     ) THEN
         EXECUTE 'ALTER TABLE aggregated_usages ADD COLUMN account_id varchar(255);';
 END IF;
+END IF;
 
     -- Update resource_uuid to 'unknown' only if NULL
-EXECUTE 'UPDATE aggregated_usages SET resource_uuid = ''unknown'' WHERE resource_uuid IS NULL;';
+IF EXISTS (
+    SELECT 1 FROM information_schema.tables
+    WHERE table_name='aggregated_usages'
+) THEN
+    EXECUTE 'UPDATE aggregated_usages SET resource_uuid = ''unknown'' WHERE resource_uuid IS NULL;';
+END IF;
 
 -- Update account_id to 'unknown' only if NULL
-EXECUTE 'UPDATE aggregated_usages SET account_id = ''test'' WHERE account_id IS NULL;';
+IF EXISTS (
+    SELECT 1 FROM information_schema.tables
+    WHERE table_name='aggregated_usages'
+) THEN
+    EXECUTE 'UPDATE aggregated_usages SET account_id = ''test'' WHERE account_id IS NULL;';
+END IF;
+
 END$$;
