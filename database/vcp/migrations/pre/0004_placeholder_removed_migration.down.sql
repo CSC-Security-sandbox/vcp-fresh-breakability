bd32b10e4937611ca65d74fdc93a3d194a65e51c
virendrawali-netapp <virendra.wali@netapp.com>
2025-11-20 11:59:16 -0700

    VSCP-3239: fix-premigration (#2053)

---
+++ database/vcp/migrations/pre/0004_placeholder_removed_migration.down.sql
@@ -0,0 +1,5 @@
+-- Rollback for placeholder migration 0004
+-- No operations needed since up migration is also empty
+
+SELECT 1;
+
---
+++ database/vcp/migrations/pre/0004_placeholder_removed_migration.up.sql
@@ -0,0 +1,7 @@
+-- This migration file was previously removed but the database had already recorded it.
+-- Created as an empty placeholder to maintain sequential integrity of the migration system.
+-- No schema changes are applied by this migration.
+
+-- Empty migration - no operations
+SELECT 1;
+
